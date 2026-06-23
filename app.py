715826d0ceafac3f6f3802636ff5aa7384a9a287
asalia1982 <124093532+asalia1982@users.noreply.github.com>
2026-06-23 10:28:50 -0600

    Fix syntax error in set_page_config call

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 
-st.set_page_config(page_title="Recicjale IA-ISC". layout="centered")
+st.set_page_config(page_title="Recicjale IA-ISC", layout="centered")
 st.title("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre entrenado")
