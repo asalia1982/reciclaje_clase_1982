7983a2dac5cb3dd8171384c6f1ce619d3f941510
asalia1982 <124093532+asalia1982@users.noreply.github.com>
2026-06-23 10:29:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ import tensorflow as tf
 from PIL import Image
 
 st.set_page_config(page_title="Recicjale IA-ISC", layout="centered")
-st.title("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026")
+st.title("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026-Asalia Zavala")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre entrenado")
