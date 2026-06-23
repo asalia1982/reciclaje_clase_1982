ecb99de251206d85b81d6139912361ef5c4eec6a
asalia1982 <124093532+asalia1982@users.noreply.github.com>
2026-06-23 10:27:37 -0600

    Add imports and Streamlit setup for image classification

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import json
+from pathlib import Path
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
 
+st.set_page_config(page_title="Recicjale IA-ISC". layout="centered")
+st.title("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026")
+st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre entrenado")
